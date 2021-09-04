dfcfbf7205a5164c84613079cb6b0ba96b0af3de
testdom <1483251067@qq.com>
2021-09-04 18:31:33 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author tianz
+ * @create ${year}-${month}-${day}${time}
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.printf("你好");
+    }
+}
