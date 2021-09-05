274fb0dbb7298d429b8a265d51b1693ada896ace
tianz <1483251067@qq.com>
2021-09-05 09:31:25 +0800

    分支第一次提交

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.printf("hello");
         System.out.printf("hot-fix");
         System.out.printf("hot-fix");
+        System.out.printf("你好master");
 
         System.out.printf("你好master");
         System.out.printf("你好master");
